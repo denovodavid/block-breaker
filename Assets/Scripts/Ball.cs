e808648f265ab8ad332cfc15c67080ed1969511c
David J Holland <stooge213@hotmail.com>
2016-12-04 21:15:19 +1100

    Add Ball physics constraints

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,5 +2,22 @@
 
 public class Ball : MonoBehaviour
 {
+    public float Speed;
+    public float RandomBounce;
+    private Rigidbody2D _Rigidbody2D;
 
+    void Start()
+    {
+        _Rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        _Rigidbody2D.velocity = Speed * _Rigidbody2D.velocity.normalized;
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(Random.Range(-RandomBounce, RandomBounce), Random.Range(-RandomBounce, RandomBounce)), ForceMode2D.Impulse);
+    }
 }
